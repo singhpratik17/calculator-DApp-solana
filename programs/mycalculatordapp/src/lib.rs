bd191e27f6007a91edc7aa96e3705032c1dbe51f
Pratik Singh <pratiktsingh17@gmail.com>
2022-01-11 20:51:07 +0530

    - Added fn definitions, structs.

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -15,18 +15,63 @@ pub mod mycalculatordapp {
     }
 
     pub fn addition(ctx:Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
-
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        Ok(())
     }
 
     pub fn multiply(ctx:Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
-
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+        Ok(())
     }
 
     pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
-
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok(())
     }
 
     pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
-
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1/num2;
+        calculator.remainder = num1 % num2;
+        Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, player = user, space = 8 + 64 + 64 + 64 + 64)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+pub struct Multiplication<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+pub struct Division<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>
+}
+
+#[account]
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64,
 }
